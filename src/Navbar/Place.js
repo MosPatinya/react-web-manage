48e21745234a3459c3d0224cb9f3b98f6f892524
Mos <mostheboy@gmail.com>
2021-11-03 01:04:33 +0700

    3/11

--- src/Navbar/Place.js
+++ src/Navbar/Place.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import DashBoard from '../components/Dashboard'
 import List_Place from '../List_place'
 import { Link } from 'react-router-dom';
+import firebase from '../config';
 import './Place.css'
 
+
 export class Place extends Component {
+
+
+    count(){
+        firebase.firestore().collection('place').get().then(function(querySnapshot){
+            console.log(querySnapshot.size)
+        })
+    }
     render() {
+        console.log(this.count())
         return (
             <div>
                 <header>
